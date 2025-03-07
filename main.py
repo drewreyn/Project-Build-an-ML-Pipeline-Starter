874638d205a589bc8164e11b6eb7a6ffcc896cb1
drewreyn <awreynolds5@gmail.com>
2025-03-06 21:02:10 -0800

    Test commit

--- main.py
+++ main.py
@@ -47,25 +47,50 @@ def go(config: DictConfig):
                     "artifact_name": "sample.csv",
                     "artifact_type": "raw_data",
                     "artifact_description": "Raw file as downloaded"
-                },
+                }
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            ##
+            mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+            entry_point="main",
+            parameters={
+                "input_artifact": "sample.csv:latest",  
+                "output_artifact": "clean_sample.csv",
+                "output_type": "cleaned_data",
+                "output_description": "Cleaned dataset after basic preprocessing",
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"]
+            }
+        )
             pass
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+            entry_point="main",
+            parameters={
+                "csv": "clean_sample.csv:latest",  
+                "ref": "clean_sample.csv:reference",
+                "kl_threshold": config["data_check"]["kl_threshold"],
+                "min_price": config["etl"]["min_price"],
+                "max_price": config["etl"]["max_price"]
+            }
+        )
             pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            mlflow.run(
+            f"{config['main']['components_repository']}/train_val_test_split",
+            'main',
+            parameters = {
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+            }
+)
             pass
 
         if "train_random_forest" in active_steps:
@@ -79,7 +104,19 @@ def go(config: DictConfig):
             # step
 
             ##################
-            # Implement here #
+            mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+            entry_point="main",
+            parameters={
+                "trainval_artifact": "trainval_data.csv:latest",
+                "val_size": config['modeling']["val_size"],
+                "random_seed": config["modeling"]["random_seed"],
+                "stratify_by": config["modeling"]["stratify_by"],
+                "rf_config": rf_config,
+                "max_tfidf_features": config["modeling"]['max_tfidf_features'],
+                "output_artifact": "random_forest_export"
+            }
+            )
             ##################
 
             pass
@@ -87,7 +124,14 @@ def go(config: DictConfig):
         if "test_regression_model" in active_steps:
 
             ##################
-            # Implement here #
+            mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+            entry_point="main",
+            parameters={
+                "mlflow_model": "random_forest_export:prod",
+                "test_dataset": "test_data.csv:latest"
+            }
+            )
             ##################
 
             pass
